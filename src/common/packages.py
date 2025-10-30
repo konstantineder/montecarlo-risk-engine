63e0007f055fb2b4b838cd8bb9ef776911a55460
Konstantin Eder <konstantin.eder@d-fine.com>
2025-10-30 15:43:08 +0100

    extended dql agent

--- src/common/packages.py
+++ src/common/packages.py
@@ -3,9 +3,9 @@ import torch
 # if torch.backends.mps.is_available():
 #     device = torch.device("mps")
 #     FLOAT = torch.float32 # MPS backend works only with float32
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    FLOAT = torch.float64 # Use float64 for better precision on GPU
-else:
-    device = torch.device("cpu")
-    FLOAT = torch.float64 # Use float64 for better precision on CPU
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+#     FLOAT = torch.float64 # Use float64 for better precision on GPU
+#else:
+device = torch.device("cpu")
+FLOAT = torch.float64 # Use float64 for better precision on CPU
